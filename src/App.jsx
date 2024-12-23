93d22e6be55ae044f52d87cf16b7f398bdc7a470
Kevin Whinnery <kevin.whinnery@gmail.com>
2024-12-23 10:56:27 -0600

    update, add bsky id

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineX } from "react-icons/ai";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className={`
+          flex flex-col items-center justify-center h-full w-full gap-4
+          md:flex-row
+        `}
+      >
+        <div>
+          <img
+            className={`
+              w-48 h-48 rounded-full object-cover shadow-2xl
+              md:w-72 md:h-auto md:rounded-lg
+            `}
+            src="/kevin.jpeg"
+            alt="Kevin Whinnery"
+          />
+        </div>
+        <div className="flex flex-col gap-2 items-start">
+          <h1 className="text-xl md:text-3xl font-bold">Kevin Whinnery</h1>
+          <p className="flex items-center gap-1">
+            Developer Experience @{" "}
+            <a
+              href="https://platform.openai.com"
+              target="_blank"
+            >
+              OpenAI
+            </a>
+          </p>
+          <ul className="flex flex-col gap-2 items-start text-sm">
+            <li className="flex items-center gap-2">
+              <AiOutlineX className="w-4 h-4" />
+              <a href="https://x.com/kevinwhinnery" target="_blank">
+                @kevinwhinnery
+              </a>
+            </li>
+            <li className="flex items-center gap-2">
+              <AiOutlineGithub className="w-4 h-4" />
+              <a href="https://github.com/kwhinnery" target="_blank">
+                @kwhinnery
+              </a>
+            </li>
+            <li className="flex items-center gap-2">
+              <AiOutlineLinkedin className="w-4 h-4" />
+              <a
+                href="https://www.linkedin.com/in/kevinwhinnery/"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
